a8ceb13e029c7e8352be20f1521aa69b53ff7502
Ashley Hozé <ashleyhoze@hotmail.fr>
2020-02-13 16:59:56 +0100

    donee

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients_json = open(url).read
+ingredients = JSON.parse(ingredients_json)
+ingredients["drinks"].each do |ingredient|
+  name = ingredient["strIngredient1"]
+  Ingredient.create!(name: name)
+end
